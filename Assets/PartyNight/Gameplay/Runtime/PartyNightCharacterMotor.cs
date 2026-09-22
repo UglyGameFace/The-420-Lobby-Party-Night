dcd80d726e843a200e419df21e2143801d140d47
UglyGameFace <85308991+UglyGameFace@users.noreply.github.com>
2026-09-22 19:15:59 -0400

    fix: remove speculative grounded-state workaround

--- Assets/PartyNight/Gameplay/Runtime/PartyNightCharacterMotor.cs
+++ Assets/PartyNight/Gameplay/Runtime/PartyNightCharacterMotor.cs
@@ -26,7 +26,6 @@ namespace PartyNight.Gameplay
         private CharacterController characterController;
         private Vector3 horizontalVelocity;
         private float verticalVelocity = GroundedVerticalSpeed;
-        private CollisionFlags lastCollisionFlags;
 
         public CharacterController CharacterController => characterController;
 
@@ -34,9 +33,7 @@ namespace PartyNight.Gameplay
             new Vector3(horizontalVelocity.x, verticalVelocity, horizontalVelocity.z);
 
         public bool IsGrounded =>
-            characterController != null &&
-            (((lastCollisionFlags & CollisionFlags.Below) != 0) ||
-             characterController.isGrounded);
+            characterController != null && characterController.isGrounded;
 
         private void Awake()
         {
@@ -53,7 +50,6 @@ namespace PartyNight.Gameplay
         {
             horizontalVelocity = Vector3.zero;
             verticalVelocity = GroundedVerticalSpeed;
-            lastCollisionFlags = CollisionFlags.None;
         }
 
         public void Tick(Vector3 desiredWorldMove, bool jumpPressed, float deltaTime)
@@ -78,7 +74,7 @@ namespace PartyNight.Gameplay
                 targetHorizontalVelocity,
                 acceleration * deltaTime);
 
-            var groundedBeforeMove = IsGrounded;
+            var groundedBeforeMove = characterController.isGrounded;
             if (groundedBeforeMove && verticalVelocity < 0f)
             {
                 verticalVelocity = GroundedVerticalSpeed;
@@ -94,8 +90,6 @@ namespace PartyNight.Gameplay
             var motion =
                 (horizontalVelocity + Vector3.up * verticalVelocity) * deltaTime;
             var collisionFlags = characterController.Move(motion);
-            lastCollisionFlags = collisionFlags;
-
             if ((collisionFlags & CollisionFlags.Below) != 0 && verticalVelocity < 0f)
             {
                 verticalVelocity = GroundedVerticalSpeed;
